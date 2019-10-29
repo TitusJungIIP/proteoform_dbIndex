36f7f880d61a2452afa94df3d327be3500ef314e
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2019-10-29 16:36:54 -0700

    new way of reporting positions of PTMs and variants

--- src/main/java/edu/scripps/yates/proteoform_dbindex/model/PTM.java
+++ src/main/java/edu/scripps/yates/proteoform_dbindex/model/PTM.java
@@ -35,6 +35,62 @@ public class PTM {
 			throws IOException {
 		final List<PTM> ptms = new ArrayList<PTM>();
 		int position = 0;
+		boolean newChangeStarts = false;
+		StringBuilder change = null;
+		for (final char aa : modifiedSequence.toCharArray()) {
+			if (change == null && ExtendedAssignMass.isAA(aa)) {
+				// is an AA
+				position++;
+			} else {
+				if (change != null) {
+					if (aa == ']') {
+						// parse change
+						if (change.toString().contains("->")) {
+							int tmp = 0;
+							final String[] split = change.toString().split("->");
+							if (split.length == 2) {
+								tmp = split[1].length();
+							}
+							final int pos = position - tmp;
+							final PTMCodeObj ptmObj = extendedAssignMass.getPTMByDescription(change.toString(), null);
+							final PTM ptm = new PTM(DynByteBuffer.toByteArray(pos)[0], ptmObj.getPtmCode());
+							ptms.add(ptm);
+						} else {
+							final Double massdiff = Double.valueOf(change.toString());
+							final PTMCodeObj ptmObj = extendedAssignMass.getPTMByDescription(change.toString(),
+									massdiff);
+							final PTM ptm = new PTM(DynByteBuffer.toByteArray(position)[0], ptmObj.getPtmCode());
+							ptms.add(ptm);
+						}
+						change = null;
+						newChangeStarts = false;
+					} else {
+						change.append(aa);
+						if (newChangeStarts) {
+							position++;
+						}
+					}
+					if (aa == '>') {
+						newChangeStarts = true;
+					}
+				} else {
+					if (aa == '[') {
+						change = new StringBuilder();
+					} else {
+						throw new IllegalArgumentException("ERROR PARSING " + modifiedSequence);
+					}
+				}
+			}
+
+		}
+
+		return ptms;
+	}
+
+	public static List<PTM> extractPTMsFromSequenceOLD(String modifiedSequence, ExtendedAssignMass extendedAssignMass)
+			throws IOException {
+		final List<PTM> ptms = new ArrayList<PTM>();
+		int position = 0;
 		StringBuilder change = null;
 		for (final char aa : modifiedSequence.toCharArray()) {
 			if (change == null && ExtendedAssignMass.isAA(aa)) {
